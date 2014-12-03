dac2e909eb28213baf4273b04761ce2f42d94391
Kevin Pfefferle <kevin@kevinpfefferle.com>
2014-12-03 15:40:55 -0500

    Update Cucumber steps for within scope

--- lib/cucumber/a11y/a11y_steps.rb
+++ lib/cucumber/a11y/a11y_steps.rb
@@ -8,26 +8,26 @@ Then(/^the page should not be accessible$/) do
   expect(page).to_not be_accessible
 end
 
-Then(/^"(.*?)" should be accessible$/) do |scope|
-  expect(page).to be_accessible_within(scope)
+Then(/^the page should be accessible within "(.*?)"$/) do |scope|
+  expect(page).to be_accessible.within(scope)
 end
 
-Then(/^"(.*?)" should not be accessible$/) do |scope|
-  expect(page).to_not be_accessible_within(scope)
+Then(/^the page should not be accessible within "(.*?)"$/) do |scope|
+  expect(page).to_not be_accessible.within(scope)
 end
 
-Then(/^the page should be accessible for tag "(.*?)"$/) do |tag|
-  expect(page).to be_accessible_for_tag(tag)
-end
-
-Then(/^the page should not be accessible for tag "(.*?)"$/) do |tag|
-  expect(page).to_not be_accessible_for_tag(tag)
-end
-
-Then(/^the page should be accessible for rule "(.*?)"$/) do |rule|
-  expect(page).to be_accessible_for_rule(rule)
-end
-
-Then(/^the page should not be accessible for tag "(.*?)"$/) do |rule|
-  expect(page).to_not be_accessible_for_rule(rule)
-end
+# Then(/^the page should be accessible for tag "(.*?)"$/) do |tag|
+#   expect(page).to be_accessible_for_tag(tag)
+# end
+#
+# Then(/^the page should not be accessible for tag "(.*?)"$/) do |tag|
+#   expect(page).to_not be_accessible_for_tag(tag)
+# end
+#
+# Then(/^the page should be accessible for rule "(.*?)"$/) do |rule|
+#   expect(page).to be_accessible_for_rule(rule)
+# end
+#
+# Then(/^the page should not be accessible for tag "(.*?)"$/) do |rule|
+#   expect(page).to_not be_accessible_for_rule(rule)
+# end
