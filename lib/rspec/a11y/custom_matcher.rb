a9e78189840d0f8b8d794ab513b8762f9d9a7a02
Kevin Pfefferle <kevin@kevinpfefferle.com>
2014-12-03 12:49:00 -0500

    Reimplement base matcher in module format

--- lib/rspec/a11y/custom_matcher.rb
+++ lib/rspec/a11y/custom_matcher.rb
@@ -1,9 +1,54 @@
+require 'json'
+
 module CustomA11yMatchers
   class BeAccessible
 
     def matches?(page)
       @page = page
-      true
+
+      execute_test_script
+      evaluate_test_results
+
+      violations_count == 0
+    end
+
+    def failure_message
+      message = "Found #{violations_count} accessibility #{violations_count == 1 ? 'violation' : 'violations'}:\n"
+      @results['violations'].each do |v|
+        message += "  #{v['help']}: #{v['helpUrl']}\n"
+        v['nodes'].each do |n|
+          message += "    #{n['html']}\n"
+          n['target'].each do |t|
+            message += "    #{t}\n"
+          end
+          message += "    #{n['failureSummary'].gsub(/\n/, "\n    ")}\n"
+        end
+      end
+      message
+    end
+
+    private
+
+    def execute_test_script
+      @page.execute_script(script_for_execute)
+    end
+
+    def script_for_execute
+      "dqre.a11yCheck(document, null, function(result){dqre.rspecResult = JSON.stringify(result);});"
+    end
+
+    def evaluate_test_results
+      # Tries #evaluate_script for Capybara, falls back to #execute_script for Watir
+      results = @page.respond_to?(:evaluate_script) ? @page.evaluate_script(script_for_evaluate) : @page.execute_script(script_for_evaluate)
+      @results = JSON.parse(results)
+    end
+
+    def script_for_evaluate
+      "(function(){return dqre.rspecResult;})()"
+    end
+
+    def violations_count
+      @results['violations'].count
     end
   end
 
--- spec/lib/custom_matcher_spec.rb
+++ spec/lib/custom_matcher_spec.rb
@@ -6,13 +6,122 @@ module CustomA11yMatchers
     before :each do
       @matcher = BeAccessible.new
       @page = double("page")
+      allow(@page).to receive(:execute_script)
     end
 
-    context "#matches?" do
+    describe "#matches?" do
 
-      it "should return true" do
-        expect(@matcher.matches?(@page)).to eq(true)
+      context "default Capybara style" do
+
+        before :each do
+          allow(@page).to receive(:evaluate_script).and_return('{"violations":[]}')
+        end
+
+        it "should execute the testing script" do
+          expect(@page).to receive(:execute_script).with(script_for_execute)
+          @matcher.matches?(@page)
+        end
+
+        it "should evaluate the test results" do
+          expect(@page).to receive(:evaluate_script).with(script_for_evaluate)
+          @matcher.matches?(@page)
+        end
+
+        it "should return true if there are no violations" do
+          expect( @matcher.matches?(@page) ).to eq(true)
+        end
+
+        it "should return false if there are violations" do
+          allow(@page).to receive(:evaluate_script).and_return('{"violations":[{}]}')
+          expect( @matcher.matches?(@page) ).to eq(false)
+        end
       end
+
+      context "alternate Watir style" do
+
+        it "should evaluate the test results" do
+          expect(@page).to receive(:execute_script).with(script_for_evaluate).and_return('{"violations":[]}')
+          @matcher.matches?(@page)
+        end
+      end
+    end
+
+    describe "#failure_message" do
+
+      before :each do
+        @results = {
+          "violations" => [
+            {
+              "help" => "Help for Violation 1",
+              "nodes" => [
+                {
+                  "target" => ["#target-1-1"],
+                  "html" => "<input id=\"target-1-1\" type=\"text\">",
+                  "failureSummary" => "Fix these for target-1-1"
+                },
+                {
+                  "target" => ["#target-1-2","#target-1-2-2"],
+                  "html" => "<input id=\"target-1-2\" type=\"text\">",
+                  "failureSummary" => "Fix these for target-1-2"
+                }
+              ],
+              "helpUrl" => "https://dequeuniversity.com/violation-1"
+            },
+            {
+              "help" => "Help for Violation 2",
+              "nodes" => [
+                {
+                  "target" => ["#target-2-1"],
+                  "html" => "<input id=\"target-2-1\" type=\"text\">",
+                  "failureSummary" => "Fix these for target-2-1"
+                },
+                {
+                  "target" => ["#target-2-2","#target-2-2-2"],
+                  "html" => "<input id=\"target-2-2\" type=\"text\">",
+                  "failureSummary" => "Fix these for target-2-2"
+                }
+              ],
+              "helpUrl" => "https://dequeuniversity.com/violation-2"
+            }
+          ]
+        }
+        allow(@page).to receive(:evaluate_script).and_return(@results.to_json)
+      end
+
+      it "should return formatted error message" do
+        @matcher.matches?(@page)
+        message = @matcher.failure_message
+
+        expect(message).to include("Found 2 accessibility violations")
+
+        @results['violations'].each do |v|
+
+          expect(message).to include(v['help'])
+          expect(message).to include(v['helpUrl'])
+
+          v['nodes'].each do |n|
+
+            expect(message).to include(n['html'])
+            expect(message).to include(n['failureSummary'])
+
+            n['target'].each do |t|
+              expect(message).to include(t)
+            end
+          end
+        end
+      end
+    end
+
+    private
+
+    def script_for_execute(context=nil, options=nil)
+      context = context ? "'#{context}'" : "document"
+      options = options ? options.to_json : 'null'
+      "dqre.a11yCheck(#{context}, #{options}, function(result){dqre.rspecResult = JSON.stringify(result);});"
+    end
+
+    def script_for_evaluate
+      "(function(){return dqre.rspecResult;})()"
     end
   end
 end
