12e39e968df5820e65e8c3acaf3c73fb6081b1c5
Jason Karns <jason@karns.name>
2015-07-16 11:31:40 -0400

    move Axe::Cucumber methods due to cucumber namespacing issues

--- lib/axe/cucumber.rb
+++ lib/axe/cucumber.rb
@@ -2,26 +2,5 @@ require 'axe/rspec/matchers'
 require 'axe/cucumber/configuration'
 require 'axe/cucumber/steps'
 
-module Axe
-  module Cucumber
-
-    def self.configuration
-      @configuration ||= Axe::Cucumber::Configuration.new
-    end
-
-    def self.configure
-      yield configuration if block_given?
-    end
-
-    def self.page(world)
-      configuration.page_from(world).tap do |page|
-        raise WebDriverError, "Configured page must implement #execute_script"  unless page.respond_to?(:execute_script)
-      end
-    end
-
-    class WebDriverError < TypeError; end
-  end
-end
-
 # expose rspec matchers to cucumber steps
 World(Axe::RSpec::Matchers)
--- lib/axe/cucumber/configuration.rb
+++ lib/axe/cucumber/configuration.rb
@@ -1,5 +1,21 @@
 module Axe
   module Cucumber
+    def self.configuration
+      @configuration ||= Axe::Cucumber::Configuration.new
+    end
+
+    def self.configure
+      yield configuration if block_given?
+    end
+
+    def self.page(world)
+      configuration.page_from(world).tap do |page|
+        raise WebDriverError, "Configured page must implement #execute_script"  unless page.respond_to?(:execute_script)
+      end
+    end
+
+    class WebDriverError < TypeError; end
+
     class Configuration
       attr_accessor :page
 
