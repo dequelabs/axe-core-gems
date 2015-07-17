c5d468c9fe0ce6674d51ae1b86930385d033220f
Jason Karns <jason@karns.name>
2015-07-17 13:05:54 -0400

    integration specs

---
+++ spec/integration_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'capybara/rspec'
+
+RSpec.configure do |c|
+  c.include Axe::RSpec::Matchers
+end
+
+feature "BeAccessible", :integration, :slow do
+  background do
+    Capybara.default_driver = :webkit
+    visit 'http://abcdcomputech.dequecloud.com'
+  end
+
+  given(:subject) { page }
+
+  scenario "check known inaccessible page" do
+    expect { expect(page).to_not be_accessible }.to_not raise_error
+    expect { expect(page).to be_accessible }.to raise_error .with_message(/Found 4 accessibility violations/)
+  end
+
+  scenario "check known accessible subtree" do
+    expect { expect(page).to be_accessible.within("#intro") }.to_not raise_error
+    expect { expect(page).to_not be_accessible.within("#intro") }.to raise_error(/Expected to find accessibility violations. None were detected./)
+  end
+
+  scenario "exclude known inaccessible subtree" do
+    expect { expect(page).to be_accessible.within("#topbar").excluding(".fl_left") }.to_not raise_error
+    expect { expect(page).to_not be_accessible.within("#topbar").excluding(".fl_left") }.to raise_error(/Expected to find accessibility violations. None were detected./)
+  end
+end
