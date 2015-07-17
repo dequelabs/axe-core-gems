585c371b7d1e7015f0b252b8e6a9b41e9ce7eecf
Jason Karns <jason@karns.name>
2015-07-16 20:20:24 -0400

    remove unecessary require_all in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ require 'rspec'
 require 'rspec/its'
 require 'axe/rspec/matchers'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
 RSpec.configure do |c|
   c.filter_run :focus => true
   c.run_all_when_everything_filtered = true
