5c4d956ce181b78304b4986c336ac39f04390243
Jason Karns <jason@karns.name>
2015-07-25 15:52:50 -0400

    skeleton module for steps

---
+++ lib/axe/cucumber/step.rb
@@ -0,0 +1,145 @@
+module Axe
+  module Cucumber
+    module Steps
+
+      # base
+
+      def be_accessible
+
+      end
+
+      def be_inaccessible
+
+      end
+
+      def be_accessible_within
+
+      end
+
+      def be_inaccessible_within
+
+      end
+
+      def be_accessible_excluding
+
+      end
+
+      def be_inaccessible_excluding
+
+      end
+
+      def be_accessible_within_but_excluding
+
+      end
+
+      def be_inaccessible_within_but_excluding
+
+      end
+
+
+      # checking preset rules for given tag
+
+      def be_accessible_according_to
+
+      end
+
+      def be_inaccessible_according_to
+
+      end
+
+      def be_accessible_within_according_to
+
+      end
+
+      def be_inaccessible_within_according_to
+
+      end
+
+      def be_accessible_excluding_according_to
+
+      end
+
+      def be_inaccessible_excluding_according_to
+
+      end
+
+      def be_accessible_within_but_excluding_according_to
+
+      end
+
+      def be_inaccessible_within_but_excluding_according_to
+
+      end
+
+
+      # checking given rules
+
+      def be_accessible_checking
+
+      end
+
+      def be_inaccessible_checking
+
+      end
+
+      def be_accessible_within_checking
+
+      end
+
+      def be_inaccessible_within_checking
+
+      end
+
+      def be_accessible_excluding_checking
+
+      end
+
+      def be_inaccessible_excluding_checking
+
+      end
+
+      def be_accessible_within_but_excluding_checking
+
+      end
+
+      def be_inaccessible_within_but_excluding_checking
+
+      end
+
+      # with custom options
+
+      def be_accessible_custom
+
+      end
+
+      def be_inaccessible_custom
+
+      end
+
+      def be_accessible_within_custom
+
+      end
+
+      def be_inaccessible_within_custom
+
+      end
+
+      def be_accessible_excluding_custom
+
+      end
+
+      def be_inaccessible_excluding_custom
+
+      end
+
+      def be_accessible_within_but_excluding_custom
+
+      end
+
+      def be_inaccessible_within_but_excluding_custom
+
+      end
+
+    end
+  end
+end
