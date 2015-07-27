536c56b7229c40f06b722875737c95044cd378c5
Jason Karns <jason@karns.name>
2015-07-27 11:47:22 -0400

    fix up BeAccessible specs now that most work is delegated

--- spec/lib/axe/matchers/be_accessible_spec.rb
+++ spec/lib/axe/matchers/be_accessible_spec.rb
@@ -3,173 +3,162 @@ require 'axe/matchers/be_accessible'
 
 module Axe::Matchers
   describe BeAccessible do
-    let(:page) { spy('page') }
-
+    let(:audit) { spy('audit') }
+    let(:results) { spy('results') }
     before :each do
-      allow(page).to receive(:evaluate_script)
-      expect(page).to receive(:execute_script).with(a_string_starting_with ("/*! aXe"))
-    end
-
-    before :each do
-      allow(page).to receive(:evaluate_script).and_return('violations' => [])
+      subject.instance_variable_set :@audit, audit
     end
 
     describe "#matches?" do
+      let(:page) { spy('page') }
 
-      it "should execute the testing script" do
-        expect(page).to receive(:execute_script).with(script_for_execute)
+      it "should run the audit against the page" do
         subject.matches?(page)
+        expect(audit).to have_received(:run_against).with(page)
       end
 
-      it "should evaluate the test results" do
-        expect(page).to receive(:evaluate_script).with(script_for_evaluate)
-        subject.matches?(page)
+      it "should save results" do
+        allow(audit).to receive(:run_against).and_return(results)
+
+        subject.matches? page
+
+        expect( subject.instance_variable_get :@results ).to be results
       end
 
-      it "should return true if there are no violations" do
-        expect( subject.matches?(page) ).to be true
+      it "should return results.passed" do
+        allow(audit).to receive(:run_against).and_return(results)
+        allow(results).to receive(:passed?).and_return(:passed)
+
+        expect( subject.matches?(page) ).to be :passed
+      end
+    end
+
+    describe "@results" do
+      before :each do
+        subject.instance_variable_set :@results, results
       end
 
-      it "should return false if there are violations" do
-        allow(page).to receive(:evaluate_script).and_return('violations' => [{}])
-        expect( subject.matches?(page) ).to be false
+      it "should be delegated #failure_message" do
+        expect(results).to receive(:failure_message).and_return(:foo)
+        expect(subject.failure_message).to eq :foo
       end
 
-      it "should retry until the a11yCheck results are ready", :slow do
-        nil_invocations = Array.new(5, nil)
-        allow(page).to receive(:evaluate_script).and_return(*nil_invocations, 'violations' => [])
-        expect( subject.matches?(page) ).to be true
-      end
-
-      it "should timeout if results aren't ready after some time", :slow do
-        allow(page).to receive(:evaluate_script) { sleep(5) and {'violations' => []} }
-        expect { subject.matches?(page) }.to raise_error Timeout::Error
+      it "should be delegated #failure_message_when_negated" do
+        expect(results).to receive(:failure_message).and_return(:foo)
+        expect(subject.failure_message_when_negated).to eq :foo
       end
     end
 
     describe "#failure_message" do
-      before :each do
-        allow(page).to receive(:evaluate_script).and_return(results)
-      end
 
-      let(:results) { {
-        "violations" => [ {
-          "help" => "V1 help",
-          "helpUrl" => "V1 url",
-          "nodes" => [ {
-            "target" => ["#target-1-1"],
-            "html" => "V1 html",
-            "any" => [ { "message" => "Fix from any 1" } ],
-            "all" => [ { "message" => "Fix from all 1" } ]
-          } ]
-        }, {
-          "help" => "V2 help",
-          "helpUrl" => "V2 url",
-          "nodes" => [ {
-            "target" => ["#target-2-1", "#target-2-2"],
-            "html" => "V2 html",
-            "any" => [ { "message" => "Fix from any 2" } ],
-            "all" => [ { "message" => "Fix from all 2" } ]
-          } ]
-        } ]
-      } }
 
-      it "should return formatted error message" do
-        subject.matches?(page)
-        subject.failure_message.tap do |message|
-          expect(message).to include("Found 2 accessibility violations")
+      # before :each do
+      #   allow(page).to receive(:evaluate_script).and_return(results)
+      # end
 
-          expect(message).to include "V1 help", "V2 help"
-          expect(message).to include "V1 url", "V2 url"
+      # let(:results) { {
+      #   "violations" => [ {
+      #     "help" => "V1 help",
+      #     "helpUrl" => "V1 url",
+      #     "nodes" => [ {
+      #       "target" => ["#target-1-1"],
+      #       "html" => "V1 html",
+      #       "any" => [ { "message" => "Fix from any 1" } ],
+      #       "all" => [ { "message" => "Fix from all 1" } ]
+      #     } ]
+      #   }, {
+      #     "help" => "V2 help",
+      #     "helpUrl" => "V2 url",
+      #     "nodes" => [ {
+      #       "target" => ["#target-2-1", "#target-2-2"],
+      #       "html" => "V2 html",
+      #       "any" => [ { "message" => "Fix from any 2" } ],
+      #       "all" => [ { "message" => "Fix from all 2" } ]
+      #     } ]
+      #   } ]
+      # } }
 
-          expect(message).to include "V1 html", "V2 html"
-          expect(message).to include "#target-1-1", "#target-2-1, #target-2-2"
+      # it "should return formatted error message" do
+      #   subject.matches?(page)
+      #   subject.failure_message.tap do |message|
+      #     expect(message).to include("Found 2 accessibility violations")
 
-          expect(message).to include "Fix from any 1", "Fix from all 1", "Fix from any 2", "Fix from all 2"
-        end
-      end
+      #     expect(message).to include "V1 help", "V2 help"
+      #     expect(message).to include "V1 url", "V2 url"
+
+      #     expect(message).to include "V1 html", "V2 html"
+      #     expect(message).to include "#target-1-1", "#target-2-1, #target-2-2"
+
+      #     expect(message).to include "Fix from any 1", "Fix from all 1", "Fix from any 2", "Fix from all 2"
+      #   end
+      # end
     end
 
     describe "#within" do
-      it "should set the context of the test script" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":[["#selector"]],"exclude":[]}'))
-        subject.within("#selector").matches?(page)
-      end
-
-      it "should accept an array of values" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":[[".include1"],[".include2"],[".include3"]],"exclude":[]}'))
-        subject.within([".include1",".include2",".include3"]).matches?(page)
-      end
-
-      it "should accept nested arrays without rewrapping" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":[[".include1"],[".include2"],["#frame1",".include3"]],"exclude":[]}'))
-        subject.within([[".include1"],[".include2"],["#frame1",".include3"]]).matches?(page)
+      it "should be delegated to @audit" do
+        subject.within(:foo)
+        expect(audit).to have_received(:include).with(:foo)
       end
     end
 
     describe "#excluding" do
-      it "should set the include/exclude context of the test script" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":[["#this"]],"exclude":[["#other"]]}'))
-        subject.within("#this").excluding("#other").matches?(page)
-      end
-
-      it "should default to document as include" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":document,"exclude":[["#other"]]}'))
-        subject.excluding("#other").matches?(page)
-      end
-
-      it "should accept an array of values" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":document,"exclude":[[".exclude1"],[".exclude2"],[".exclude3"]]}'))
-        subject.excluding([".exclude1",".exclude2",".exclude3"]).matches?(page)
-      end
-
-      it "should accept nested arrays without rewrapping" do
-        expect(page).to receive(:execute_script).with(script_for_execute('{"include":document,"exclude":[[".exclude1"],[".exclude2"],["#frame1",".exclude3"]]}'))
-        subject.excluding([[".exclude1"],[".exclude2"],["#frame1",".exclude3"]]).matches?(page)
+      it "should be delegated to @audit" do
+        subject.excluding(:foo)
+        expect(audit).to have_received(:exclude).with(:foo)
       end
     end
 
     describe "#for_tag" do
-      it "should pass the tag options to the script" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"tag","values":["wcag2a"]}}'))
-        subject.for_tag("wcag2a").matches?(page)
+      it "should be delegated to @audit" do
+        subject.for_tag(:foo)
+        expect(audit).to have_received(:rules_by_tags)
+      end
+
+      it "should accept a single tag" do
+        subject.for_tag(:foo)
+        expect(audit).to have_received(:rules_by_tags).with([:foo])
+      end
+
+      it "should accept many tags" do
+        subject.for_tag(:foo, :bar)
+        expect(audit).to have_received(:rules_by_tags).with([:foo, :bar])
       end
 
       it "should accept an array of tags" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"tag","values":["wcag2a","wcag2aa","section508"]}}'))
-        subject.for_tag(["wcag2a","wcag2aa", "section508"]).matches?(page)
+        subject.for_tag([:foo, :bar])
+        expect(audit).to have_received(:rules_by_tags).with([:foo, :bar])
       end
 
-      it "should accept variadic arguments" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"tag","values":["wcag2a","wcag2aa","section508"]}}'))
-        subject.for_tag("wcag2a","wcag2aa", "section508").matches?(page)
-      end
-
-      it "should have a plural alias" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"tag","values":["wcag2a","wcag2aa","section508"]}}'))
-        subject.for_tags("wcag2a","wcag2aa", "section508").matches?(page)
+      it "should have the plural form #for_tags" do
+        subject.for_tags(:foo, :bar)
+        expect(audit).to have_received(:rules_by_tags).with([:foo, :bar])
       end
     end
 
     describe "#for_rule" do
-      it "should pass the rule options to the script" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"rule","values":["label"]}}'))
-        subject.for_rule("label").matches?(page)
+      it "should be delegated to @audit" do
+        subject.for_rule(:foo)
+        expect(audit).to have_received(:run_only_rules)
+      end
+
+      it "should accept a single rule" do
+        subject.for_rule(:foo)
+        expect(audit).to have_received(:run_only_rules).with([:foo])
+      end
+
+      it "should accept many rules" do
+        subject.for_rule(:foo, :bar)
+        expect(audit).to have_received(:run_only_rules).with([:foo, :bar])
       end
 
       it "should accept an array of rules" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"rule","values":["label","blink","list"]}}'))
-        subject.for_rule(["label","blink", "list"]).matches?(page)
+        subject.for_rule([:foo, :bar])
+        expect(audit).to have_received(:run_only_rules).with([:foo, :bar])
       end
 
-      it "should accept variadic args" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"rule","values":["label","blink","list"]}}'))
-        subject.for_rule("label","blink", "list").matches?(page)
-      end
-
-      it "should have an plural alias" do
-        expect(page).to receive(:execute_script).with(script_for_execute('document', '{"runOnly":{"type":"rule","values":["label","blink","list"]}}'))
-        subject.for_rules("label","blink", "list").matches?(page)
+      it "should have the plural form #for_rules" do
+        subject.for_rules(:foo, :bar)
+        expect(audit).to have_received(:run_only_rules).with([:foo, :bar])
       end
     end
 
