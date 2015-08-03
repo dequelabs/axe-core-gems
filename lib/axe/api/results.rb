696cec1eddc700e3a29eceb4a88a7d79270dfb7e
Jason Karns <jason@karns.name>
2015-08-03 17:54:23 -0400

    extract Rule to own file

--- lib/axe/api/results.rb
+++ lib/axe/api/results.rb
@@ -1,33 +1,10 @@
 require 'virtus'
-require 'axe/api/results/checked_node'
+require 'axe/api/results/rule'
 
 module Axe
   module API
-    class ValueObject
-      include Virtus.value_object
-    end
-
     class Results
       include Virtus.value_object
-
-      class Rule < ValueObject
-        values do
-          attribute :id, Symbol
-          attribute :description, String
-          attribute :help, String
-          attribute :helpUrl, String
-          attribute :impact, Symbol
-          attribute :tags, Array[Symbol]
-          attribute :nodes, Array[CheckedNode]
-        end
-
-        def failure_message(index)
-          <<-MSG
-          #{index+1}) #{help}: #{helpUrl}
-          #{nodes.map(&:failure_message).join("\n")}
-          MSG
-        end
-      end
 
       values do
         attribute :url
