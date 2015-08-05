bfc373a18009e138405e3aa6325872c4e75e75a7
Jason Karns <jason@karns.name>
2015-08-05 13:03:47 -0400

    test the failure message at Audit level

--- spec/lib/axe/api/audit_spec.rb
+++ spec/lib/axe/api/audit_spec.rb
@@ -4,8 +4,20 @@ require 'axe/api/audit'
 module Axe::API
   describe Audit do
     let(:subject) { described_class.new invocation, results }
-    let(:invocation) { spy('invocation') }
+    let(:invocation) { "inny vacation" }
     let(:results) { spy('results') }
+
+    before :each do
+      allow(results).to receive(:failure_message).and_return("results failure message")
+    end
+
+    its(:failure_message) do
+      is_expected.to eq "results failure message\nInvocation: inny vacation"
+    end
+
+    its(:failure_message_when_negated) do
+      is_expected.to eq "Expected to find accessibility violations. None were detected.\nInvocation: inny vacation"
+    end
 
     context "when there are no violations" do
       before :each do
