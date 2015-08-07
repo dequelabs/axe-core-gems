c5d8b193567cbafb081b77e2b581397eae13d4f0
Jason Karns <jason@karns.name>
2015-08-07 12:46:32 -0400

    intention revealing methods

--- lib/axe/page.rb
+++ lib/axe/page.rb
@@ -8,7 +8,19 @@ module Axe
     def_delegators :@browser, :execute_script, :execute_async_script
 
     def initialize(browser)
-      @browser = ::WebDriverScriptAdapter::ExecuteAsyncScriptAdapter.wrap ::WebDriverScriptAdapter::ExecEvalScriptAdapter.wrap browser
+      @browser = wrap_exec_async wrap_exec_eval browser
+    end
+
+    private
+
+    # ensure driver has #execute_async_script
+    def wrap_exec_async(driver)
+      ::WebDriverScriptAdapter::ExecuteAsyncScriptAdapter.wrap driver
+    end
+
+    # ensure driver has #execute_script and #evaluate_script
+    def wrap_exec_eval(driver)
+      ::WebDriverScriptAdapter::ExecEvalScriptAdapter.wrap driver
     end
   end
 end
