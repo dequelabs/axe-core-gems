fcee151058fb6e095bb9ca2bd5f8fb48e6c8167e
Jason Karns <jason@karns.name>
2015-08-07 12:38:23 -0400

    move async script adapter out of axe namespace

--- lib/webdriver_script_adapter/execute_async_script_adapter.rb
+++ lib/webdriver_script_adapter/execute_async_script_adapter.rb
@@ -2,35 +2,33 @@ require 'dumb_delegator'
 require 'securerandom'
 require 'timeout'
 
-module Axe
-  class Page
-    class ExecuteAsyncScriptAdapter < ::DumbDelegator
-      def self.wrap(driver)
-        new driver
-      end
+module WebDriverScriptAdapter
+  class ExecuteAsyncScriptAdapter < ::DumbDelegator
+    def self.wrap(driver)
+      new driver
+    end
 
-      def execute_async_script(script, *args)
-        results = async_results_identifier(::SecureRandom.uuid)
-        execute_script async_wrapper(script, args, results)
-        wait_until { evaluate_script results }
-      end
+    def execute_async_script(script, *args)
+      results = async_results_identifier(::SecureRandom.uuid)
+      execute_script async_wrapper(script, args, results)
+      wait_until { evaluate_script results }
+    end
 
-      private
+    private
 
-      def async_results_identifier(key)
-        "window['#{key}']"
-      end
+    def async_results_identifier(key)
+      "window['#{key}']"
+    end
 
-      def async_wrapper(script, args, resultsIdentifier)
-        ";(function(){ #{script} })(#{args.join(', ')}, function(returnValue){ #{resultsIdentifier} = returnValue; });"
-      end
+    def async_wrapper(script, args, resultsIdentifier)
+      ";(function(){ #{script} })(#{args.join(', ')}, function(returnValue){ #{resultsIdentifier} = returnValue; });"
+    end
 
-      def wait_until
-        # TODO make the timeout limit configurable
-        ::Timeout.timeout(3) do
-          sleep(0.1) until value = yield
-          value
-        end
+    def wait_until
+      # TODO make the timeout limit configurable
+      ::Timeout.timeout(3) do
+        sleep(0.1) until value = yield
+        value
       end
     end
   end
