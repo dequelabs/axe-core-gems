6047ce2aff1aeb04f23691381ea417403826eda8
Jason Karns <jason@karns.name>
2015-08-10 15:00:53 -0400

    define visit method on World singleton instead of module mixin

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -1,15 +1,11 @@
 require_relative 'env'
 require 'selenium-webdriver'
 
-# module so that our test steps can be driver-agnostic
-World(Module.new do
-  def visit(url)
-    @browser.navigate.to url
-  end
-end)
-
 Before do
   @browser = Selenium::WebDriver.for $browser
+  define_singleton_method :visit do |url|
+    @browser.navigate.to url
+  end
 end
 
 After do
--- features/support/watir.rb
+++ features/support/watir.rb
@@ -1,15 +1,11 @@
 require_relative 'env'
 require 'watir-webdriver'
 
-# module so that our test steps can be driver-agnostic
-World(Module.new do
-  def visit(url)
-    @browser.goto url
-  end
-end)
-
 Before do
   @browser = Watir::Browser.new $browser
+  define_singleton_method :visit do |url|
+    @browser.goto url
+  end
 end
 
 After do
