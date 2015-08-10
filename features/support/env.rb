d558c3cc2e83c6d099a961205557caf2064bc606
Jason Karns <jason@karns.name>
2015-08-10 15:02:04 -0400

    remove old config

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,6 @@
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 
-require File.dirname(__FILE__) + "/fixtures/a11y_test_page"
+# require File.dirname(__FILE__) + "/fixtures/a11y_test_page"
 
 require 'axe/cucumber/step_definitions'
 
