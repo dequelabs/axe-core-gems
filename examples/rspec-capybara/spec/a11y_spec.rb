d0d470377f8990e1cafa5682024bd1e865814a9e
Jason Karns <jason@karns.name>
2015-08-17 11:34:05 -0400

    add rspec+capybara example using capybara's DSL

--- examples/rspec-capybara/spec/a11y_spec.rb
+++ examples/rspec-capybara/spec/a11y_spec.rb
@@ -4,7 +4,9 @@ require 'capybara/rspec'
 require 'axe/rspec'
 #######
 
-describe "ABCD CompuTech", :type => :feature, :driver => :selenium do
+
+# Typical example using standard RSpec dsl
+describe "ABCD CompuTech (RSpec DSL)", :type => :feature, :driver => :selenium do
   before :each do
     visit 'http://abcdcomputech.dequecloud.com/'
   end
@@ -18,6 +20,25 @@ describe "ABCD CompuTech", :type => :feature, :driver => :selenium do
   end
 
   it "is known to have an inaccessible sub-tree (should fail)" do
+    expect(page).to be_accessible.within '#broken'
+  end
+end
+
+# Typical example using Capybara's DSL
+feature "ABCD CompuTech (Capybara DSL)", :driver => :selenium do
+  background do
+    visit 'http://abcdcomputech.dequecloud.com/'
+  end
+
+  scenario "is known to be inaccessible, should fail" do
+    expect(page).to be_accessible
+  end
+
+  scenario "is known to have an accessible sub-tree (should pass)" do
+    expect(page).to be_accessible.within '#working'
+  end
+
+  scenario "is known to have an inaccessible sub-tree (should fail)" do
     expect(page).to be_accessible.within '#broken'
   end
 end
