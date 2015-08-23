4ddae22d480fc23663dfc98f06458af3aa9a68c5
Jason Karns <jason@karns.name>
2015-08-23 14:14:33 -0400

    rename methods on Options

--- lib/axe/api/options.rb
+++ lib/axe/api/options.rb
@@ -6,11 +6,8 @@ module Axe
     class Options
       extend Forwardable
 
-      def_delegator :@rules, :by_tags, :rules_by_tags
-      def_delegator :@rules, :run_only, :run_only_rules
-      def_delegator :@rules, :run, :run_rules
-      def_delegator :@rules, :skip, :skip_rules
-      def_delegator :@custom, :merge!, :custom_options
+      def_delegators :@rules, :according_to, :checking, :checking_only, :skipping
+      def_delegator :@custom, :merge!, :with_options
 
       attr_reader :rules, :custom
 
--- spec/lib/axe/api/options_spec.rb
+++ spec/lib/axe/api/options_spec.rb
@@ -4,44 +4,44 @@ require 'axe/api/options'
 module Axe::API
   describe Options do
     before :each do
-      allow(subject.rules).to receive(:by_tags)
-      allow(subject.rules).to receive(:run_only)
-      allow(subject.rules).to receive(:run)
-      allow(subject.rules).to receive(:skip)
+      allow(subject.rules).to receive(:according_to)
+      allow(subject.rules).to receive(:checking)
+      allow(subject.rules).to receive(:checking_only)
+      allow(subject.rules).to receive(:skipping)
     end
 
-    describe "#rules_by_tags" do
-      it "should be delegated to @rules.by_tag" do
-        subject.rules_by_tags([:foo])
-        expect(subject.rules).to have_received(:by_tags).with([:foo])
+    describe "#according_to" do
+      it "should be delegated to @rules" do
+        subject.according_to([:foo])
+        expect(subject.rules).to have_received(:according_to).with([:foo])
       end
     end
 
-    describe "#run_only_rules" do
-      it "should be delegated to @rules.run_only" do
-        subject.run_only_rules([:foo])
-        expect(subject.rules).to have_received(:run_only).with([:foo])
+    describe "#checking_only" do
+      it "should be delegated to @rules" do
+        subject.checking_only([:foo])
+        expect(subject.rules).to have_received(:checking_only).with([:foo])
       end
     end
 
-    describe "#run_rules" do
-      it "should be delegated to @rules.run" do
-        subject.run_rules([:foo])
-        expect(subject.rules).to have_received(:run).with([:foo])
+    describe "#checking" do
+      it "should be delegated to @rules" do
+        subject.checking([:foo])
+        expect(subject.rules).to have_received(:checking).with([:foo])
       end
     end
 
-    describe "#skip_rules" do
-      it "should be delegated to @rules.skip" do
-        subject.skip_rules([:foo])
-        expect(subject.rules).to have_received(:skip).with([:foo])
+    describe "#skipping" do
+      it "should be delegated to @rules" do
+        subject.skipping([:foo])
+        expect(subject.rules).to have_received(:skipping).with([:foo])
       end
     end
 
-    describe "#custom_options" do
+    describe "#with_options" do
       context "without existing custom options" do
         it "should merge additional custom options" do
-          subject.custom_options :foo => :bar
+          subject.with_options :foo => :bar
           expect(subject.custom).to eq :foo => :bar
         end
       end
@@ -52,7 +52,7 @@ module Axe::API
         end
 
         it "should merge additional custom options" do
-          subject.custom_options :baz => :qux
+          subject.with_options :baz => :qux
           expect(subject.custom).to eq :foo => :bar, :baz => :qux
         end
       end
