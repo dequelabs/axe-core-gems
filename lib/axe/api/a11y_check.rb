3384078eb3533173d08a386117f06f2c241df99a
Jason Karns <jason@karns.name>
2015-08-23 14:14:44 -0400

    rename methods on A11yCheck

--- lib/axe/api/a11y_check.rb
+++ lib/axe/api/a11y_check.rb
@@ -17,11 +17,7 @@ module Axe
 
       extend Forwardable
       def_delegators :@context, :within, :excluding
-      def_delegator :@options, :rules_by_tags, :according_to
-      def_delegator :@options, :run_rules, :checking
-      def_delegator :@options, :run_only_rules, :checking_only
-      def_delegator :@options, :skip_rules, :skipping
-      def_delegator :@options, :custom_options, :with_options
+      def_delegators :@options, :according_to, :checking, :checking_only, :skipping, :with_options
 
       extend ChainMail::Chainable
       chainable :within, :excluding, :according_to, :checking, :checking_only, :skipping, :with_options
