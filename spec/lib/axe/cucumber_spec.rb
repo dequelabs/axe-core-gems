e3598612bf4e7a0206b155cd055b16f5fe79372b
Jason Karns <jason@karns.name>
2015-08-25 13:00:35 -0400

    new pending specs

---
+++ spec/lib/axe/cucumber_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Axe
+  describe "cucumber" do
+    pending "mixes the DSL into World"
+  end
+end
---
+++ spec/lib/axe/expectation_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module Axe
+  describe AccessibilityExpectation do
+    pending "lots of API specs"
+
+    pending "can be used directly in manually-defined step defs without arg-parsing"
+  end
+end
