47e0d5597dda834e5334a44badc7ad31cbf9af7c
Jason Karns <jason@karns.name>
2016-02-29 22:10:50 -0500

    load axe into iframes (selenium)

--- lib/axe/loader.rb
+++ lib/axe/loader.rb
@@ -10,12 +10,21 @@ module Axe
     def call
       @page.execute_script @lib.source unless already_loaded?
       Axe.configuration.run_after_load_hook @lib
+      load_into_iframes
     end
 
     private
 
     def already_loaded?
       @page.evaluate_script @lib.already_loaded?
+    end
+
+    def load_into_iframes
+      @page.find_elements(:tag_name, "iframe").each do |iframe|
+        @page.switch_to.frame iframe
+        call
+        @page.switch_to.default_content # this might need to be in an 'ensure'
+      end
     end
   end
 end
