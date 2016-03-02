c3b0412bf305a1c3fbfcc154a74ca093e17a7de5
Jason Karns <jason@karns.name>
2016-03-01 21:25:15 -0500

    add specs

--- spec/lib/axe/loader_spec.rb
+++ spec/lib/axe/loader_spec.rb
@@ -4,13 +4,19 @@ require 'axe/loader'
 module Axe
   describe Loader do
     subject(:loader) { described_class.new(page, lib) }
-    let(:page) { spy('page') }
+    let(:page) { spy('page', evaluate_script: false) }
     let(:lib) { spy('lib', source: 'libsource') }
 
     describe "call" do
       it "should evaluate lib's source in the context of the given page" do
         loader.call
         expect(page).to have_received(:execute_script).with('libsource')
+      end
+
+      it "should not re-load lib if already loaded" do
+        allow(page).to receive(:evaluate_script).and_return(true)
+        loader.call
+        expect(page).not_to have_received(:execute_script).with('libsource')
       end
 
       it "should run the after_load hook" do
