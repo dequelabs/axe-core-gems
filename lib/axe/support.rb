2adc70a6754628bcbe4b29ea222eb15a8075f62c
Jason Karns <jason@karns.name>
2016-03-03 13:54:10 -0500

    refactor Support#page_from

--- lib/axe/support.rb
+++ lib/axe/support.rb
@@ -2,16 +2,14 @@ require 'axe'
 
 module Axe
   module Support
+    WEBDRIVER_NAMES = [ :@page, :@browser, :@driver, :@webdriver ]
 
     module_function
 
     def page_from(world)
       configured_page_object ||
-        page_by_name(Axe.configuration.page, world) ||
-        page_by_name(:@page, world) ||
-        page_by_name(:@browser, world) ||
-        page_by_name(:@driver, world) ||
-        page_by_name(:@webdriver, world) ||
+        page_by_method_name(world) ||
+        page_by_ivar_name(world) ||
         NullWebDriver.new
     end
 
@@ -21,11 +19,13 @@ module Axe
       Axe.configuration.page unless Axe.configuration.page.is_a?(String) || Axe.configuration.page.is_a?(Symbol)
     end
 
-    def self.page_by_name(page, world)
-      if world.respond_to? page
-        world.__send__(page)
-      else
-        world.instance_variable_get(page) rescue nil
+    def self.page_by_method_name(world)
+      world.__send__(Axe.configuration.page) if world.respond_to?(Axe.configuration.page)
+    end
+
+    def self.page_by_ivar_name(world)
+      if ivar_name = ([Axe.configuration.page.to_sym].concat(WEBDRIVER_NAMES) & world.instance_variables).first
+        world.instance_variable_get(ivar_name)
       end
     end
 
