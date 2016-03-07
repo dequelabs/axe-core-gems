ef4ec02cb6d421b9fef840f78a395d90500ad0e6
Jason Karns <jason@karns.name>
2016-03-07 14:59:18 -0500

    more flexible version module

--- lib/axe/version.rb
+++ lib/axe/version.rb
@@ -1,3 +1,12 @@
 module Axe
-  VERSION = "1.1.1"
+  module VERSION
+    MAJOR=1
+    MINOR=1
+    PATCH=1
+    PRE=nil
+
+    def self.to_s
+      [MAJOR, MINOR, PATCH, PRE].compact.join(".")
+    end
+  end
 end
