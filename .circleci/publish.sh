b0c4ae3d149562752005182066cdbcb2fe1e9d37
Stephen Mathieson <me@stephenmathieson.com>
2018-09-27 11:09:07 -0400

    ci: setup automated releases for "develop" and "master" commits (#46)
    * ci: setup automated releases for "develop" and "master" commits
    
    This patch adds CircleCI configuration for automatically publishing "pre" and "production" gems to Artifactory when commits land into develop and master.
    
    * ci: setup ruby gems creds
    
    * ci: remove "basic" from publish script

---
+++ .circleci/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Setup `gem` credentials.
+# Based on https://medium.com/@pezholio/publishing-rubygems-using-circle-ci-2-0-1dbf06ae9942.
+mkdir ~/.gem
+echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
+
+if [ "$CIRCLE_BRANCH" = "develop" ]; then
+  rm -rf pkg
+  # Add a ".pre.SHA" suffix to the version number.
+  sed -i '' "s/spec\.version *= *\"\(.*\)\"/spec.version = \"\1.pre.$(git rev-parse --short HEAD)\"/" axe-matchers.gemspec
+  rake build
+  gem push $(pkg/*.gem)
+elif [ "$CIRCLE_BRANCH" = "master" ]; then
+  rm -rf pkg
+  rake build
+  gem push $(pkg/*.gem)
+else
+  echo "Invalid branch. Refusing to publish." 1>&2
+  exit 1
+fi
