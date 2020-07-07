9d621bf2c01cae3eb2f43cf9d45dbc653e3ba948
jkodu <jey.nandakumar@gmail.com>
2020-07-07 21:19:16 +0100

    test: add specs

---
+++ src/packages/axe-core-rspec/spec/axe-core-rspec_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+module Axe
+  describe "RSpec" do
+    pending "can be configured automatically"
+
+    pending "can use the DSL"
+
+    pending "can use Matcher directly"
+
+    pending "can be used with MiniTest"
+
+    pending "can be used with MiniTest + https://github.com/wojtekmach/minitest-matchers"
+
+    pending "can be used with MiniTest + https://github.com/rmm5t/minitest-matchers_vaccine"
+  end
+end
