bdc9c2a5906caec4aa4622004619ce198491398b
jkodu <jey.nandakumar@gmail.com>
2020-07-08 02:11:58 +0100

    add trspec tests

---
+++ src/packages/axe-core-cucumber/spec/axe-core-cucumber_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+require_relative "../axe-core-cucumber"
+
+module AxeCucumber
+  describe Step do
+    describe "::create_for" do
+      it "should create a Step" do
+        allow(Axe::FindsPage).to receive_message_chain("in.page")
+        expect(described_class.create_for :world).to be_a Step
+      end
+
+      it "creates the step with the page from the given world" do
+        allow(Step).to receive(:new)
+        expect(Axe::FindsPage).to receive_message_chain("in.page").and_return(:page)
+
+        described_class.create_for :world
+
+        expect(Step).to have_received(:new).with(:page)
+      end
+    end
+
+    pending "#assert_accessibility"
+
+    pending "can be invoked directly in manually-defined step defs (to get arg-parsing OOTB)"
+  end
+end
