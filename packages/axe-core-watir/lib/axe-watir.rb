c80ba039f7907bcd859d042a09c3ba64d6963a29
jkodu <jey.nandakumar@gmail.com>
2020-07-16 18:44:38 +0100

    use axe-core-api

--- packages/axe-core-watir/lib/axe-watir.rb
+++ packages/axe-core-watir/lib/axe-watir.rb
@@ -1,13 +1,11 @@
 require "watir"
-require_relative "../../../common/axe/configuration"
+require "axe/configuration"
 
 module AxeWatir
-
   # configure method
   # - which takes an optional argument browser
   # - and a configuration block optional for Axe
   def self.configure(browser = :firefox)
-
     # instantiate axe configuration (singleton) with defaults or given config
     if !block_given?
       raise Exception.new "Please provide a configure block for AxeWatir"
