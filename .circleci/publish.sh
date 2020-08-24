818e5db620938b9ec7a857b0821c378d2c949774
jkodu <jey.nandakumar@gmail.com>
2020-08-24 20:04:56 +0100

    ci: Setup cananry and prod release

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-
 set -e
 
-# Setup `gem` credentials.
+# Setup `gem` credentials
 # Based on https://medium.com/@pezholio/publishing-rubygems-using-circle-ci-2-0-1dbf06ae9942.
 mkdir ~/.gem
-echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" >~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 
-if [ "$CIRCLE_BRANCH" = "develop" ]; then
-  rm -rf pkg
-  # Add a ".pre.SHA" suffix to the version number.
-  sed -i -e "s/spec\.version\s*=\s'\(.*\)'/spec.version='\1.pre.$(git rev-parse --short HEAD)'/" axe-matchers.gemspec 
-  rake build
-  gem push $(ls pkg/*.gem)
-elif [ "$CIRCLE_BRANCH" = "master" ]; then
-  rm -rf pkg
-  rake build
-  gem push $(ls pkg/*.gem)
+# Helper fn to update version.rb
+setup_version() {
+  echo "
+  module AxeCoreGems
+    VERSION = "\"$1\""
+  end
+  " >version.rb
+}
+
+# get version number
+version=$(cat package.json |
+  grep version |
+  head -1 |
+  awk -F: '{ print $2 }' |
+  sed 's/[",]//g' |
+  tr -d '[[:space:]]')
+
+# version suffix provided?
+if [ ! -z "$1" ]; then
+  setup_version "$version.$1"
 else
-  echo "Invalid branch. Refusing to publish." 1>&2
-  exit 1
+  setup_version $version
 fi
+
+# publish
+rake publish
