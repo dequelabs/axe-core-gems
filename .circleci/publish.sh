acca0cb7f177234ffeb02211c5bd194495c71a98
jkodu <jey.nandakumar@gmail.com>
2020-08-24 20:20:20 +0100

    ci: update publish script

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,7 +3,6 @@ set -e
 
 # Setup `gem` credentials
 # Based on https://medium.com/@pezholio/publishing-rubygems-using-circle-ci-2-0-1dbf06ae9942.
-mkdir ~/.gem
 echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" >~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 
