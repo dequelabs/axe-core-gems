d5fbcce411887ce8a4cf600d6392048012f4a21c
jkodu <jey.nandakumar@gmail.com>
2020-08-27 16:15:30 +0100

    ci: use geckodriver install script

--- .circleci/install-geckodriver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-curl -L -o "/tmp/geckodriver.tar.gz" "https://github.com/mozilla/geckodriver/releases/download/v$GECKODRIVER_VERSION/geckodriver-v$GECKODRIVER_VERSION-linux64.tar.gz"
-tar -xvzf /tmp/geckodriver.tar.gz -C /tmp/
-sudo mv "/tmp/geckodriver" "/opt/geckodriver"
-chmod +x /opt/geckodriver
-sudo ln -sf "/opt/geckodriver" "/usr/local/bin/geckodriver" 
