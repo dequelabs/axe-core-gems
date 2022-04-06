d7f6d3635b876188432a10bf9db35933aa4f5bfb
Stephen Margheim <stephen.margheim@gmail.com>
2022-04-06 15:32:33 +0200

    fix: Allow `none` values for ValueObject, since can be set by CheckedNode result type (#228)

--- packages/axe-core-api/lib/axe/api/value_object.rb
+++ packages/axe-core-api/lib/axe/api/value_object.rb
@@ -3,7 +3,7 @@ require 'virtus'
 module Axe
   module API
     class ValueObject
-      include ::Virtus.value_object mass_assignment: false, nullify_blank: true
+      include ::Virtus.value_object mass_assignment: false, nullify_blank: false
     end
   end
 end
