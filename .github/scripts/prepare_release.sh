b08a116b3a5ee1b87f755f127e7d7432d0e4c270
michael-siek <michaelsiek094@gmail.com>
2022-10-26 15:50:19 -0400

    ci: automate release process

---
+++ .github/scripts/prepare_release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail on first error.
+set -e
+
+releaseLevel="$1"
+
+oldVersion="$(node -pe 'require("./package.json").version')"
+npx standard-version --release-as "$releaseLevel" --skip.commit=true --skip.changelog=true --skip.tag=true
+newVersion="$(node -pe 'require("./package.json").version')"
+
+sed -i -e "s/  VERSION\\s*=\\s*\"$oldVersion\"/  VERSION = \"$newVersion\"/" version.rb
+
+npx conventional-changelog-cli -p angular -i CHANGELOG.md -s
