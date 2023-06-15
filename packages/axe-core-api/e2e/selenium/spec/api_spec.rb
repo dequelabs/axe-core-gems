4a88bb77d90104feb3dbe750dd26e2b5391f3ebf
AdnoC <adam.r.cutler@gmail.com>
2023-06-15 13:22:20 -0400

    chrome opts?

--- packages/axe-core-api/e2e/selenium/spec/api_spec.rb
+++ packages/axe-core-api/e2e/selenium/spec/api_spec.rb
@@ -6,6 +6,8 @@ require "axe/api/run"
 
 options = Selenium::WebDriver::Chrome::Options.new
 # options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 $driver = Selenium::WebDriver.for :chrome, options: options
 
 Run = Axe::API::Run
