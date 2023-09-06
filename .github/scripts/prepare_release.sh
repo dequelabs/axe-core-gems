ac92f4fe2b476e36076612629578efca390c6925
Adam Cutler <adam.r.cutler@gmail.com>
2023-09-06 17:34:55 -0400

    Single stage prep release

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -6,9 +6,7 @@ set -e
 releaseLevel="$1"
 
 oldVersion="$(node -pe 'require("./package.json").version')"
-npx standard-version@"$STD_VERSION_VERSION" --release-as "$releaseLevel" --skip.commit=true --skip.changelog=true --skip.tag=true
+npx standard-version --release-as "$releaseLevel" --skip.commit=true --skip.tag=true
 newVersion="$(node -pe 'require("./package.json").version')"
 
 sed -i -e "s/  VERSION\\s*=\\s*\"$oldVersion\"/  VERSION = \"$newVersion\"/" version.rb
-
-npx conventional-changelog-cli@"$CHANGELOG_VERSION" -p angular -i CHANGELOG.md -s
