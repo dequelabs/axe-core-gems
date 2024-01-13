8ad70bff4d9c6e50998ede739834979504aa7a86
Michael <me@michaelsiek.com>
2024-01-12 19:49:53 -0500

    ci: generate changelogs with `commit-and-tag-version` (#363)

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -10,9 +10,9 @@ oldVersion="$(node -pe 'require("./package.json").version')"
 # If no release level is specified, let commit-and-tag-version handle versioning
 if [ -z "$releaseLevel" ] 
 then
-  npx commit-and-tag-version --skip.commit=true --skip.changelog=true --skip.tag=true
+  npx commit-and-tag-version --skip.commit=true --skip.tag=true
 else
-  npx commit-and-tag-version --release-as "$releaseLevel" --skip.commit=true --skip.changelog=true --skip.tag=true
+  npx commit-and-tag-version --release-as "$releaseLevel" --skip.commit=true --skip.tag=true
 fi
 
 newVersion="$(node -pe 'require("./package.json").version')"
