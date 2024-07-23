6860b2c950a7e72a595ff547e433ab7e960c0e1c
Scott Ries <scottmries@gmail.com>
2024-07-23 11:20:35 -0400

    replace comment

--- packages/axe-core-api/e2e/selenium/spec/api_spec.rb
+++ packages/axe-core-api/e2e/selenium/spec/api_spec.rb
@@ -5,6 +5,7 @@ require "axe/core"
 require "axe/api"
 
 options = Selenium::WebDriver::Chrome::Options.new
+# options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 $driver = Selenium::WebDriver.for :chrome, options: options
