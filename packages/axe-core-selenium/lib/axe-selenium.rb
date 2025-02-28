b5eb55b0ec75266fcf7bb22726ceb1ee71457148
Scott Ries <scottmries@gmail.com>
2025-02-28 14:03:22 -0500

    tests

--- packages/axe-core-selenium/lib/axe-selenium.rb
+++ packages/axe-core-selenium/lib/axe-selenium.rb
@@ -6,8 +6,8 @@ module AxeSelenium
   # - which takes an optional argument browser
   # - an optional options (sic) object to configure the underlying driver
   # - and a configuration block optional for Axe
-  def self.configure(browser = :firefox, opts = {})
-    # instantiate axe configuration (singleton) with defaults or given config
+  def self.configure(browser = :firefox, opts = nil)
+    # instantiate axe configuration (singleton) with defaults or given config: opts
     if !block_given?
       raise Exception.new "Please provide a configure block for AxeSelenium"
     end
@@ -24,7 +24,7 @@ module AxeSelenium
 
   private
 
-  def self.get_driver(*args)
-    Selenium::WebDriver.for *args
+  def self.get_driver(browserSymbol, opts)
+      Selenium::WebDriver.for browserSymbol, options: opts
   end
 end
